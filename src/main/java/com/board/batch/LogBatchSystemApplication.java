ab6455c9adaa3cdfcaacf30352381e1d77a81baf
jangyoona <olozg@naver.com>
2026-01-01 21:51:59 +0900

    Add initial project files

---
+++ src/main/java/com/board/batch/LogBatchSystemApplication.java
@@ -0,0 +1,16 @@
+package com.board.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class LogBatchSystemApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(LogBatchSystemApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/board/batch/LogBatchSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogBatchSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
