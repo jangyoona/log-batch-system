28069470afed190cd724e8daa2bd344057c440e8
jangyoona <olozg@naver.com>
2026-01-02 17:26:03 +0900

    refactor split sidebar and footer from page layout

--- src/main/java/com/board/batch/LogBatchSystemApplication.java
+++ src/main/java/com/board/batch/LogBatchSystemApplication.java
@@ -3,9 +3,11 @@ package com.board.batch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@MapperScan("com.board.batch.mapper")
+@EnableTransactionManagement
+@MapperScan({"com.board.batch.common.mapper", "com.board.batch.user.mapper"})
 public class LogBatchSystemApplication {
 
 	public static void main(String[] args) {
