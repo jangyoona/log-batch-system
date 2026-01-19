0bd1fc4bfc1d4366b73f3cd665a5bb5eda504c5a
jangyoona <olozg@naver.com>
2026-01-19 22:06:23 +0900

    chore: downgrade Spring Boot version and and AOP dependency

--- src/test/java/com/board/batch/LogBatchSystemApplicationTests.java
+++ src/test/java/com/board/batch/LogBatchSystemApplicationTests.java
@@ -2,8 +2,10 @@ package com.board.batch;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "spring.main.allow-bean-definition-overriding=true")
 class LogBatchSystemApplicationTests {
 
 	@Test
